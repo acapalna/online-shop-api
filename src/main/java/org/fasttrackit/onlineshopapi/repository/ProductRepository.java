3b65580918b8658f5c69b1d0b0985346a1f99029
acapalna <augustin.capalna@yahoo.com>
2019-06-03 19:00:35 +0300

    Add named queries

--- src/main/java/org/fasttrackit/onlineshopapi/repository/ProductRepository.java
+++ src/main/java/org/fasttrackit/onlineshopapi/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.fasttrackit.onlineshopapi.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 //Long is wrapper for primitive type long
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -11,4 +12,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     //queries derives from method names springBoot
     Page<Product> findByNameContaining(String partialName, Pageable pageable);
     Page<Product> findByNameContainingAndQuantityGreaterThanEqual(String partialName, int minQuantity, Pageable pageable);
+
+    // named query using JPQL (java persistence query language
+    @Query("SELECT product FROM Product product WHERE name LIKE '%?1'") // '%?1' primul parametru
+    // named query using native sql
+//    @Query(value = "SELECT * FROM Product product WHERE name LIKE '%?1'", nativeQuery = true)
+    Page<Product> findByPartialName(String partialName, Pageable pageable);
+
+
 }
